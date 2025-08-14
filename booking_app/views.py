a667b6de65cd081ffc0031c1be6c132845442b69
BruhWaxie <efirestmax@gmail.com>
2025-08-14 17:11:03 +0300

    Add room details view and update index template with Bootstrap card layout

--- booking_app/views.py
+++ booking_app/views.py
@@ -4,3 +4,7 @@ from booking_app.models import Room, Booking
 def home(request):
     rooms = Room.objects.all()
     return render(request, 'index.html', context={'rooms':rooms})
+
+def room_info(request, room_id):
+    room = Room.objects.get(id=room_id)
+    return render(request, 'room-info.html', context={'room':room})
--- booking_project/urls.py
+++ booking_project/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from booking_app.views import home
-
+from booking_app.views import *
 urlpatterns = [
-    path('', home, name='home'),
     path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    path('room-info/<int:room_id>',room_info, name='room_info')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
